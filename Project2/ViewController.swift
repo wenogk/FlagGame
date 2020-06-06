79fddee882b230dd0f179eb8dff52d79f9bb4a3d
wenogk <drf325@nyu.edu>
2020-06-06 17:33:28 +0400

    p1

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -11,9 +11,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    var countries = [String]();
+    var score: Int = 0;
     override func viewDidLoad() {
         super.viewDidLoad()
+        countries.append("estonia");
+        countries.append("france");
+        countries.append("germany");
+        countries.append("ireland");
+        countries.append("italy");
+        countries.append("monaco");
+        countries.append("nigeria");
+        countries.append("poland");
+        countries.append("russia");
+        countries.append("spain");
+        countries.append("us");
+        countries.append("uk");
         // Do any additional setup after loading the view.
+    }
+    
+    func askQuestion() {
+        
     }
 
 
