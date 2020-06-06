c986572de499b88632ece202ffcf6e12d4d12a7a
wenogk <drf325@nyu.edu>
2020-06-06 17:53:53 +0400

    ask question function

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -32,10 +32,23 @@ class ViewController: UIViewController {
         countries.append("us");
         countries.append("uk");
         // Do any additional setup after loading the view.
+        
+        button1.layer.borderWidth = 1;
+        button2.layer.borderWidth = 1;
+        button3.layer.borderWidth = 1;
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor;
+        button2.layer.borderColor = UIColor.lightGray.cgColor;
+        button3.layer.borderColor = UIColor.lightGray.cgColor;
+        
+        
+        askQuestion()
     }
     
     func askQuestion() {
-        
+        button1.setImage(UIImage(named: countries[0]), for: .normal);
+        button2.setImage(UIImage(named: countries[1]), for: .normal);
+        button3.setImage(UIImage(named: countries[2]), for: .normal);
     }
 
 
