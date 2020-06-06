d60ee844a321606e70fb94e72a749fff31b5325e
wenogk <drf325@nyu.edu>
2020-06-06 22:17:18 +0400

    random display of flags + scoring + selecting a random correct flag

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     
     var countries = [String]();
     var score: Int = 0;
+    var correctAnswer: Int = 0;
     override func viewDidLoad() {
         super.viewDidLoad()
         countries.append("estonia");
@@ -46,11 +47,30 @@ class ViewController: UIViewController {
     }
     
     func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
         button1.setImage(UIImage(named: countries[0]), for: .normal);
         button2.setImage(UIImage(named: countries[1]), for: .normal);
         button3.setImage(UIImage(named: countries[2]), for: .normal);
+        title = countries[correctAnswer].uppercased();
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if(sender.tag == correctAnswer) {
+            score += 1;
+            title="Correct"
+        } else{
+            score -= 1;
+            title = "Wrong"
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert);
+        
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        
+    }
+    
 }
 
